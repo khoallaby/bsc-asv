d4ae2be43cea1f389c6259e80eac204e6166a611
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-17 12:53:19 -0700

    init work of 'discover amazing trips'

---
+++ resources/views/partials/sections/discover-amazing-trips.blade.php
@@ -0,0 +1,18 @@
+<section class="container-fluid discover-amazing-trips">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>DISCOVER OUR AMAZING TRIPS</h2>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque lauda ntium, totam rem aperiam, eaque ipsa quae ab illo inventore ver it.</p>
+            </div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-2 amazing-trip">
+                <a href="#">
+                    <img src="" /><br />
+                    6 DAY RAFTING
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
