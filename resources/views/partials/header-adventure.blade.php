26d472055f2df477a316eef2ccc76c428e9e6631
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-17 02:04:08 -0700

    blue header - adventure

---
+++ resources/views/partials/header-adventure.blade.php
@@ -0,0 +1,10 @@
+<section class="container-fluid header-adventure">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 adventure-text">
+                FOLLOW THE
+            </div>
+            <div class="col-6"></div>
+        </div>
+    </div>
+</section>
