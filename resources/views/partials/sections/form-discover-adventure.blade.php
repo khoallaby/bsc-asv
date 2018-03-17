5df55b31b4175b9f0ca178ed8237b62d08bfbe51
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-17 10:20:25 -0700

    form - discover your adventure

---
+++ resources/views/partials/sections/form-discover-adventure.blade.php
@@ -0,0 +1,11 @@
+<section class="container-fluid discover-adventure">
+    <div class="container">
+        <h2>HAVE SOME QUESTIONS ABOUT YOUR TRIP?</h2>
+        <form>
+            <input type="text" /> <input type="text" /> <input type="text" /> <br />
+            <textarea></textarea><br />
+            <input type="submit" class="btn-primary" />
+
+        </form>
+    </div>
+</section>
