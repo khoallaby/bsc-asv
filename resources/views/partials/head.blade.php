1f1a1153a769c6af0fa63af02748ec5aa1749d2f
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-18 20:33:19 -0700

    hardcode font awesome for now

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,5 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed|Saira+Extra+Condensed" rel="stylesheet">
+  <!-- delete later -->
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
   @php(wp_head())
 </head>
