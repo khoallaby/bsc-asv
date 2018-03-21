ad3a78c766e941188a493dd8aeb257d02c4a05e0
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-21 09:23:11 -0700

    fix up river menus

--- resources/views/partials/sections/amazing-trips/river-menu.blade.php
+++ resources/views/partials/sections/amazing-trips/river-menu.blade.php
@@ -7,5 +7,19 @@
       <h2><span class="primary-color">RIVER MENU</span> Day 1 & 2</h2>
       <a href="#" class="btn btn-primary white">View Menu</a>
     </div>
+    <div class="col-md-4 river-menu green">
+      <div class="river-menu-logo">
+        <img src="@asset('images/logo-white.png')" class="img-fluid" />
+      </div>
+      <h2><span class="primary-color">RIVER MENU</span> Day 3 & 4</h2>
+      <a href="#" class="btn btn-primary white">View Menu</a>
+    </div>
+    <div class="col-md-4 river-menu orange">
+      <div class="river-menu-logo">
+        <img src="@asset('images/logo-white.png')" class="img-fluid" />
+      </div>
+      <h2><span class="primary-color">RIVER MENU</span> Day 5 & 6</h2>
+      <a href="#" class="btn btn-primary white">View Menu</a>
+    </div>
   </div>
 </section>
