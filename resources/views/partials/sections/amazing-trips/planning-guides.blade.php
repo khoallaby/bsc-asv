8788b33ce99747404c77df4e8f0ae4ab3a024a55
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-21 09:36:41 -0700

    single amazing trip - planning guides

--- resources/views/partials/sections/amazing-trips/planning-guides.blade.php
+++ resources/views/partials/sections/amazing-trips/planning-guides.blade.php
@@ -1,7 +1,41 @@
 <section class="container-fluid planning-guides">
-  <div class="row no-gutters">
-    <div class="col-12">
-
+  <div class="container">
+    <div class="row">
+      <div class="col-12 planning-guide-content">
+        <h2>Planning Guides</h2>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque lauda ntium, totam rem aperiam, eaque ipsa quae ab illo inventore ver it.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet d.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3 planning-guide">
+        <a href="#">
+          <img src="@asset('images/icons/planning-guide.png')" class="img-fluid" /><br />
+          <h3>Which season is right for me</h3>
+        </a>
+      </div>
+      <div class="col-md-3 planning-guide">
+        <a href="#">
+          <img src="@asset('images/icons/planning-guide.png')" class="img-fluid" /><br />
+          <h3>Which season is right for me</h3>
+        </a>
+      </div>
+      <div class="col-md-3 planning-guide">
+        <a href="#">
+          <img src="@asset('images/icons/planning-guide.png')" class="img-fluid" /><br />
+          <h3>Which season is right for me</h3>
+        </a>
+      </div>
+      <div class="col-md-3 planning-guide">
+        <a href="#">
+          <img src="@asset('images/icons/planning-guide.png')" class="img-fluid" /><br />
+          <h3>Which season is right for me</h3>
+        </a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        @include('partials.book-your-adventure')
+      </div>
     </div>
   </div>
 </section>
