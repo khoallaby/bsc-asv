c4ec03e88a0c9c3d6d075bf928745ff482d3ed99
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-20 19:55:33 -0700

    featured image on single post page

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,5 +1,11 @@
 <article @php(post_class())>
   <header>
+      @php
+      echo get_the_post_thumbnail( $post->ID, 'large', array(
+          'class' => 'img-fluid',
+          //'srcset' => ''
+      ) );
+      @endphp
     <h2 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
     @include('partials/entry-meta')
   </header>
