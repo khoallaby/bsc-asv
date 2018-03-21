b08c10684f6a4a21a342abe96fafdf51a6dc5f39
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-21 00:17:22 -0700

    get sidebar aligned side by side

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,12 @@
     @include('partials.header')
     @yield('pre-content')
     <div class="wrap container" role="document">
-      <div class="content">
-        <main class="main">
+      <div class="row content">
+        <main class="main {{ App\display_sidebar() ? 'col-md-9' : 'col-12' }}">
           @yield('content')
         </main>
         @if (App\display_sidebar())
-          <aside class="sidebar">
+          <aside class="sidebar col-md-3">
             @include('partials.sidebar')
           </aside>
         @endif
