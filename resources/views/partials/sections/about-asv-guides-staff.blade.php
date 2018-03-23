554fab1026d82338a94f778e6091f6b622226004
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-22 21:42:53 -0700

    about page

---
+++ resources/views/partials/sections/about-asv-guides-staff.blade.php
@@ -0,0 +1,24 @@
+<section class="container-fluid asv-guides-staff">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h2>MEET THE ASV GUIDES AND STAFF</h2>
+      </div>
+    </div>
+    <div class="row">
+      <ul class="list-inline">
+        <li class="col-md-3 asv-staff">
+          <img src="@asset('images/icons/planning-guide.png')" class="img-fluid" height="135" width="135" /><br />
+          <h3 class="name">brad frei</h3>
+          <h4 class="position">Owner & Host</h4>
+          <p class="bio-short">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
+          <a href="#asv-staff-ID" class="btn btn-primary">Read More</a>
+          <div id="asv-staff-ID" class="container" style="display: none;">
+            <div class="title">brad frei - Owner & Host</div>
+            <div class="bio-long">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
---
+++ resources/views/template-about.blade.php
@@ -0,0 +1,26 @@
+{{--
+  Template Name: About Page
+--}}
+
+@extends('layouts.app')
+
+
+
+@section('content')
+  @while(have_posts()) @php(the_post())
+    <section class="content-top">
+      <h2>WHY CHOOSE ADVENTURE SUN VALLEY</h2>
+      <p>@include('partials.content-page')</p>
+      @include('partials.book-your-adventure')
+    </section>
+  @endwhile
+@endsection
+
+
+
+@section('post-content')
+  @include('partials.header-adventure', [ 'title' => 'Follow the' ])
+
+  @include('partials.sections.about-asv-guides-staff')
+  @include('partials.sections.map-salmon-river')
+@endsection
