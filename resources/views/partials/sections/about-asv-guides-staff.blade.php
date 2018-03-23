f4c6675bf2a5419328995de63d7ce44a40e990ca
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-22 22:33:06 -0700

    styling for about asv-staff cards

--- resources/views/partials/sections/about-asv-guides-staff.blade.php
+++ resources/views/partials/sections/about-asv-guides-staff.blade.php
@@ -6,19 +6,24 @@
       </div>
     </div>
     <div class="row">
-      <ul class="list-inline">
-        <li class="col-md-3 asv-staff">
-          <img src="@asset('images/icons/planning-guide.png')" class="img-fluid" height="135" width="135" /><br />
-          <h3 class="name">brad frei</h3>
-          <h4 class="position">Owner & Host</h4>
-          <p class="bio-short">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
-          <a href="#asv-staff-ID" class="btn btn-primary">Read More</a>
-          <div id="asv-staff-ID" class="container" style="display: none;">
-            <div class="title">brad frei - Owner & Host</div>
-            <div class="bio-long">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</div>
+        @for ($i = 0; $i < 10; $i++)
+        <div class="col-md-3">
+          <div class="asv-staff-container">
+            <div class="asv-staff">
+              <img src="@asset('images/icons/planning-guide.png')" class="staff-pic" height="135" width="135" />
+              <h3 class="staff-name">brad frei</h3>
+              <h4 class="staff-position">Owner & Host</h4>
+              <p class="bio-short">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
+              <a href="#asv-staff-ID{{ $i }}" class="btn btn-primary">Read More</a>
+              <div id="asv-staff-ID{{ $i }}" class="container" style="display: none;">
+                <div class="title">brad frei - Owner & Host</div>
+                <div class="bio-long">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</div>
+              </div>
+            </div>
           </div>
-        </li>
-      </ul>
+        </div>
+        @endfor
+
     </div>
   </div>
 </section>
