ffdc0bd3021d1856b3a0da8545702d36ece52312
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-23 00:43:50 -0700

    acf for river menu

--- resources/views/partials/sections/amazing-trips/river-menu.blade.php
+++ resources/views/partials/sections/amazing-trips/river-menu.blade.php
@@ -5,21 +5,21 @@
         <img src="@asset('images/logo-white.png')" class="img-fluid" />
       </div>
       <h2><span class="primary-color">RIVER MENU</span> Day 1 & 2</h2>
-      <a href="#" class="btn btn-primary white">View Menu</a>
+      <a href="{{ get_field('river_menu_day_1_2') }}" class="btn btn-primary white">View Menu</a>
     </div>
     <div class="col-md-4 river-menu green">
       <div class="river-menu-logo">
         <img src="@asset('images/logo-white.png')" class="img-fluid" />
       </div>
       <h2><span class="primary-color">RIVER MENU</span> Day 3 & 4</h2>
-      <a href="#" class="btn btn-primary white">View Menu</a>
+      <a href="{{ get_field('river_menu_day_3_4') }}" class="btn btn-primary white">View Menu</a>
     </div>
     <div class="col-md-4 river-menu orange">
       <div class="river-menu-logo">
         <img src="@asset('images/logo-white.png')" class="img-fluid" />
       </div>
       <h2><span class="primary-color">RIVER MENU</span> Day 5 & 6</h2>
-      <a href="#" class="btn btn-primary white">View Menu</a>
+      <a href="{{ get_field('river_menu_day_5_6') }}" class="btn btn-primary white">View Menu</a>
     </div>
   </div>
 </section>
