575f9ac9158360ca88424c499a6c20efe69f1963
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-23 22:31:01 -0700

    simplify sidebar custom page template

--- resources/views/template-sidebar.blade.php
+++ resources/views/template-sidebar.blade.php
@@ -2,11 +2,4 @@
   Template Name: Sidebar Template
 --}}
 
-@extends('layouts.app')
-
-@section('content')
-  @while(have_posts()) @php(the_post())
-    @include('partials.page-header')
-    @include('partials.content-page')
-  @endwhile
-@endsection
+@include('page')
