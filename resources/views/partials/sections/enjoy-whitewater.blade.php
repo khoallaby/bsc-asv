d84e915f62a4207affa513bd8ad2712934478a7f
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-24 23:38:49 -0700

    acf for - enjoy whitewater

--- resources/views/partials/sections/enjoy-whitewater.blade.php
+++ resources/views/partials/sections/enjoy-whitewater.blade.php
@@ -1,18 +1,18 @@
 <section class="container-fluid enjoy-whitewater no-gutters">
     <div class="row">
-        <div class="col-md-6 enjoy-whitewater-left">
-            <h2>Enjoy the <span class="teal-color">best wilderness whitewater river</span> in the world!</h2>
+        <div class="col-md-6 enjoy-whitewater-left" style="background-image: url('{{ the_field('enjoy_5050_left_background') }}');">
+            <h2>{{ the_field('enjoy_5050_left_title') }}</h2>
             <div class="row">
-                <div class="col-2 offset-1 text-center">
+                <div class="col-md-2 offset-1 text-center">
                     <img src="@asset('images/logo-white.png')" class="img-fluid" />
                 </div>
-                <div class="col-8">
-                  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullam. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tinreet dolore magna aliquam erat volutpat.</p>
+                <div class="col-md-8">
+                  <p>{{ the_field('enjoy_5050_left_content') }}</p>
                   @include('partials.book-your-adventure', [ 'color' => 'white' ])
                 </div>
 
             </div>
         </div>
-        <div class="col-md-6 enjoy-whitewater-right"></div>
+        <div class="col-md-6 enjoy-whitewater-right" style="background-image: url('{{ the_field('enjoy_5050_right_background') }}');"></div>
     </div>
 </section>
