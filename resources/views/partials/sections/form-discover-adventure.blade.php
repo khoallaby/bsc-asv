845d351252b0ded555aa3f64f358be445470a75e
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-25 00:39:46 -0700

    gforms - contact - styling

--- resources/views/partials/sections/form-discover-adventure.blade.php
+++ resources/views/partials/sections/form-discover-adventure.blade.php
@@ -1,11 +1,14 @@
 <section class="container-fluid discover-adventure">
-    <div class="container">
-        <h2>HAVE SOME QUESTIONS ABOUT YOUR TRIP?</h2>
-        <form>
-            <input type="text" /> <input type="text" /> <input type="text" /> <br />
-            <textarea></textarea><br />
-            <input type="submit" class="btn btn-primary white" />
-
-        </form>
+  <div class="container">
+    <div class="row">
+      <h2>HAVE SOME QUESTIONS ABOUT YOUR TRIP?</h2>
+      {!! do_shortcode('[gravityform id="1"]') !!}
     </div>
+    {{--
+    <form>
+      <input type="text" /> <input type="text" /> <input type="text" /> <br />
+      <textarea></textarea><br />
+      <input type="submit" class="btn btn-primary white" />
+    </form>--}}
+  </div>
 </section>
