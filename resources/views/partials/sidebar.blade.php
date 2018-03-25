1715b11b1472adee0b23467c51042d6520dcc27a
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-25 11:18:11 -0700

    permanent "follow us" on blog sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,8 +1,15 @@
 <aside class="sidebar col-md-3 {{ ( is_home() || is_single() ) ? 'sidebar-blog' : 'sidebar-primary' }}">
-  @php
-    if( is_home() || is_single() )
-      dynamic_sidebar('sidebar-blog');
-    else
-      dynamic_sidebar('sidebar-primary');
-  @endphp
+  <?php if( is_home() || is_single() ) { ?>
+    <section class="widget widget_text">
+      <h3>Follow Us</h3>
+      <div class="textwidget">
+        @include('partials.social-media')
+      </div>
+    </section>
+    <?php
+    dynamic_sidebar('sidebar-blog');
+  } else {
+    dynamic_sidebar('sidebar-primary');
+  }
+  ?>
 </aside>
