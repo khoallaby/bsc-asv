3b3375e701c1ac6449d05a03febc688f4036a082
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-24 23:26:47 -0700

    add rev slider for home slider

--- resources/views/partials/sections/home-slider.blade.php
+++ resources/views/partials/sections/home-slider.blade.php
@@ -1,5 +1,6 @@
 <section class="container-fluid home-slider no-gutters">
-    <div class="row">
-        <img src="@asset('images/slider-1.jpg')" />
-    </div>
+  <div class="row">
+    {!! do_shortcode('[rev_slider alias="home"]') !!}
+    {{--<img src="@asset('images/slider-1.jpg')" />--}}
+  </div>
 </section>
