7119fe0eb3fe79a4767315a4cd5fd15e716db095
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-25 01:22:49 -0700

    fix 'find your adventure'

--- resources/views/partials/sections/find-your-adventure.blade.php
+++ resources/views/partials/sections/find-your-adventure.blade.php
@@ -12,7 +12,7 @@
             $background_image = get_sub_field('background_image');
             #var_dump($image);
             #$text_align = $i % 2 ? 'text-left' : 'text-right';
-            $learn_more = get_sub_field('learn_more_link') ? sprintf( '<br /><a href="%s" class="btn btn-primary white">Learn More</a>', get_sub_field('learn_more_link') ) : '';
+            $learn_more = get_sub_field('learn_more_link') ? sprintf( '<a href="%s" class="btn btn-primary white">Learn More</a>', get_sub_field('learn_more_link') ) : '';
             if( $i % 2 ) {
                 $bg_class = 'show-xxs';
                 $bg_class2 = 'hide-xxs';
@@ -21,7 +21,7 @@
             }
             $div_image = '<div class="col-xs-12 col-md-6 fya-background ' . $bg_class . '" style="background-image: url(\'' . $opposing_image['url'] . '\');"><img src="' . $opposing_image['url'] . '" class="img-fluid" /></div>';
             $div_image2 = '<div class="col-xs-12 col-md-6 fya-background ' . $bg_class2 . '" style="background-image: url(\'' . $opposing_image['url'] . '\');"><img src="' . $opposing_image['url'] . '" class="img-fluid" /></div>';
-            $div_text = sprintf('<div class="col-xs-12 col-md-6 fya-content hcenter vcenter" style="background-image: url(%s);"><div class="pad"><h2>%s</h2>%s %s</div></div>',
+            $div_text = sprintf('<div class="col-xs-12 col-md-6 fya-content hcenter vcenter" style="background-image: url(%s);"><div class="pad"><h2>%s</h2><p>%s</p> %s</div></div>',
                 #$text_align,
                 $background_image['url'],
                 get_sub_field('title'),
