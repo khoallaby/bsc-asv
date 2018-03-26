9fdd8dc429cd6f48e0ea533a1cb990958ca4aeaa
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-25 17:46:58 -0700

    faqs accordion

--- resources/views/template-faqs.blade.php
+++ resources/views/template-faqs.blade.php
@@ -7,6 +7,32 @@
 @section('content')
   @while(have_posts()) @php(the_post())
     @include('partials.page-header')
-    @include('partials.content-page')
-  @endwhile
+
+    @if ( have_rows('faqs') )
+      @php( $i = 0 )
+      <div id="accordion" class="accordion panel-group" role="tablist" aria-multiselectable="true">
+      @while ( have_rows('faqs') ) @php(the_row())
+        @php( $i++ )
+        <div class="panel panel-default">
+          <div class="panel-heading" role="tab" id="heading-{{ $i }}">
+            <h2 class="panel-title">
+              <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse-{{ $i }}" aria-expanded="false" aria-controls="collapse-{{ $i }}">
+                {{ get_sub_field('title') }}
+              </a>
+            </h2>
+
+          </div>
+          <div id="collapse-{{ $i }}" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading-{{ $i }}">
+            <div class="panel-body">{{ get_sub_field('content') }}</div>
+          </div>
+        </div>
+      @endwhile
+    @endif
+
+
+
+
+
+</div>
+@endwhile
 @endsection
