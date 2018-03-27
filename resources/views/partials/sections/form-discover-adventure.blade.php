1fae5db5b941f6e8a719bd46f99463cd3ce6e91b
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-27 00:53:06 -0700

    discover adventure form update to use separate images

--- resources/views/partials/sections/form-discover-adventure.blade.php
+++ resources/views/partials/sections/form-discover-adventure.blade.php
@@ -1,14 +1,15 @@
 <section class="container-fluid discover-adventure">
   <div class="container">
     <div class="row">
-      <h2>HAVE SOME QUESTIONS ABOUT YOUR TRIP?</h2>
-      {!! do_shortcode('[gravityform id="1"]') !!}
+
+      <div class="col-md-12 logo"><img src="@asset('images/logo-footer.png')" class="img-fluid"/></div>
+      <div class="col-md-12 discover-your-adventure-text">
+        <img src="@asset('images/discover-your-adventure.png')" class="img-fluid"/><br />
+        <h2>HAVE SOME QUESTIONS ABOUT YOUR TRIP?</h2>
+      </div>
+      <div class="col-md-12 col-push-2">
+        {!! do_shortcode('[gravityform id="1"]') !!}
+      </div>
     </div>
-    {{--
-    <form>
-      <input type="text" /> <input type="text" /> <input type="text" /> <br />
-      <textarea></textarea><br />
-      <input type="submit" class="btn btn-primary white" />
-    </form>--}}
   </div>
 </section>
