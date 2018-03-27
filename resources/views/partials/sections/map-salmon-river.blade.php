12dafcd71fc894a398b72cd70a367fe018768896
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-27 14:00:07 -0600

    Commented out "container" to allow map image to fill the space

--- resources/views/partials/sections/map-salmon-river.blade.php
+++ resources/views/partials/sections/map-salmon-river.blade.php
@@ -1,7 +1,7 @@
 <?php if( get_field('salmon_river_map') ): ?>
 <section class="container-fluid map-salmon-river">
-    <div class="container">
+    <!--<div class="container">-->
         <img src="<?php the_field('salmon_river_map'); ?>" class="img-fluid" />
-    </div>
+    <!--</div>-->
 </section>
 <?php endif; ?>
