261439d50099620da79b47cc13bb90bc1a235a71
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-27 10:51:56 -0700

    fix accordion, show 1 only at a time

--- resources/views/template-faqs.blade.php
+++ resources/views/template-faqs.blade.php
@@ -16,13 +16,13 @@
         <div class="panel panel-default">
           <div class="panel-heading" role="tab" id="heading-{{ $i }}">
             <h2 class="panel-title">
-              <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse-{{ $i }}" aria-expanded="false" aria-controls="collapse-{{ $i }}">
+              <a class="collapsed" data-toggle="collapse" href="#collapse-{{ $i }}" aria-expanded="false" aria-controls="collapse-{{ $i }}">
                 {{ get_sub_field('title') }}
               </a>
             </h2>
 
           </div>
-          <div id="collapse-{{ $i }}" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading-{{ $i }}">
+          <div id="collapse-{{ $i }}" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading-{{ $i }}" data-parent="#accordion">
             <div class="panel-body">{{ get_sub_field('content') }}</div>
           </div>
         </div>
