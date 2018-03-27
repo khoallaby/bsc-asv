389298af4ab67625ea1fdac4c5255eb52a70768b
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-27 11:53:59 -0600

    Add Salmon River Map ACF

--- resources/views/partials/sections/map-salmon-river.blade.php
+++ resources/views/partials/sections/map-salmon-river.blade.php
@@ -1,5 +1,7 @@
+<?php if( get_field('salmon_river_map') ): ?>
 <section class="container-fluid map-salmon-river">
     <div class="container">
-        <img src="@asset('images/map-salmon-river.png')" class="img-fluid" />
+        <img src="<?php the_field('salmon_river_map'); ?>" class="img-fluid" />
     </div>
 </section>
+<?php endif; ?>
