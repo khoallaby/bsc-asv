dd5b197074c414d2632c69348cca3fc890d04772
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-28 10:23:49 -0600

    Make images links that open in a new window to Instagram

--- resources/views/partials/sections/amazing-trips/amazing-trip-gallery.blade.php
+++ resources/views/partials/sections/amazing-trips/amazing-trip-gallery.blade.php
@@ -9,7 +9,7 @@
         $image = get_sub_field('image');
     ?>
     <div class="col-sm-6 col-md-4 col-lg-2">
-      <img src="{{ $image['sizes']['gallery-square-thumbnail'] }}" class="img-fluid" />
+      <a href="https://www.instagram.com/middleforksalmonriverrafting/" target="_blank"><img src="{{ $image['sizes']['gallery-square-thumbnail'] }}" class="img-fluid" /></a>
     </div>
     <?php
       endwhile;
