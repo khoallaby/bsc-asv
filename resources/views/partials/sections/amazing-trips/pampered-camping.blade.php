d7d1d71fda05ca8a8dcbbf7f7472e7b95bf085f8
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-28 10:28:22 -0600

    Update pampered-camping.blade.php

--- resources/views/partials/sections/amazing-trips/pampered-camping.blade.php
+++ resources/views/partials/sections/amazing-trips/pampered-camping.blade.php
@@ -2,8 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <h2>WHAT IS PAMPERED CAMPING?</h2>
-        <p>Catus error sit voluptatem accusantium doloremque lauda ntium, totam rem aperiam, eaque ip vol utpat n hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+        <?php the_field('pampered_camping'); ?>
         @include('partials.book-your-adventure', [ 'color' => 'white' ])
       </div>
     </div>
