3d32bbb4cc180fd2d5c6accde73a64bf74cb5fee
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-27 23:48:43 -0700

    blog sidebar - make social icons big

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="sidebar col-md-3 {{ ( is_home() || is_single() ) ? 'sidebar-blog' : 'sidebar-primary' }}">
   <?php if( is_home() || is_single() ) { ?>
-    <section class="widget widget_text">
+    <section class="widget widget_text widget-follow-us">
       <h3>Follow Us</h3>
       <div class="textwidget">
         @include('partials.social-media')
