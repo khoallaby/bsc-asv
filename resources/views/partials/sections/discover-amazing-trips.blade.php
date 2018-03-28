06532817bad7802b99c8ba70f111a95210fad7af
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-28 09:57:13 -0600

    Update discover-amazing-trips.blade.php

--- resources/views/partials/sections/discover-amazing-trips.blade.php
+++ resources/views/partials/sections/discover-amazing-trips.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-12">
               <?php
-                if ( have_posts() ) : while ( have_posts() ) : the_post(); the_content(); endwhile; endif;
+                the_content();
               ?>
             </div>
         </div>
