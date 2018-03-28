5b9101f425acbdee6594641c585b3c1f35fda0a2
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-28 09:53:15 -0600

    Update discover-amazing-trips.blade.php

--- resources/views/partials/sections/discover-amazing-trips.blade.php
+++ resources/views/partials/sections/discover-amazing-trips.blade.php
@@ -2,9 +2,9 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-              @php
+              <?php
                 if ( have_posts() ) : while ( have_posts() ) : the_post(); the_content(); endwhile; endif;
-              @endphp
+              ?>
             </div>
         </div>
         <div class="row no-gutters">
