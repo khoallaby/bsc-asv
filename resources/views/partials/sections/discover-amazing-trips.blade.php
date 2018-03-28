99ee656649a85145f03e3e8e4d1f72d8beb6cdb6
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-28 10:00:21 -0600

    Fixed loop issue

--- resources/views/partials/sections/discover-amazing-trips.blade.php
+++ resources/views/partials/sections/discover-amazing-trips.blade.php
@@ -9,7 +9,6 @@
                     if ( have_posts() ) : while ( have_posts() ) : the_post(); the_content(); endwhile; endif;
                 }
               @endphp
-
             </div>
         </div>
         <div class="row no-gutters">
