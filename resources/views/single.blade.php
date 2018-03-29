4148384068b7c4e57331aaf0780c7b798adfdaa2
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-29 00:09:20 -0700

    pagination for single post

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -4,4 +4,9 @@
   @while(have_posts()) @php(the_post())
     @include('partials.content-single-'.get_post_type())
   @endwhile
+
+  <div class="post-pagination">
+    {{ previous_post_link('%link', '&laquo; Previous Post') }}
+    {{ next_post_link('%link', 'Next Post &raquo;') }}
+  </div>
 @endsection
