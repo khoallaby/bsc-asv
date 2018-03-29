d45d376973e4784d3a04ca8f4b9f0207c3e9a3f9
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-29 07:59:03 -0700

    salmon river acf is global

--- resources/views/partials/sections/map-salmon-river.blade.php
+++ resources/views/partials/sections/map-salmon-river.blade.php
@@ -1,7 +1,7 @@
-<?php if( get_field('salmon_river_map') ): ?>
+<?php if( get_field('salmon_river_map', 'options') ): ?>
 <section class="container-fluid map-salmon-river">
     <!--<div class="container">-->
-        <img src="<?php the_field('salmon_river_map'); ?>" class="img-fluid" />
+        <img src="<?php the_field('salmon_river_map', 'options'); ?>" class="img-fluid" />
     <!--</div>-->
 </section>
 <?php endif; ?>
