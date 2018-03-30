54a97454e3a67a0dc055e662eabaa8ee6c92fe69
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-03-29 23:47:08 -0700

    add drop shadow behind <header>

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -18,18 +18,6 @@
 
     </div>
   </div>
-  <div class="container">
-    <nav class="nav-primary">
-      {{--
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-      --}}
-    </nav>
-  </div>
-
-
-
 
 
   <div class="container">
