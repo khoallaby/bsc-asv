ad324851c6bf02bc6f72fe556995edc4c3b4d08f
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-04-11 19:30:11 -0600

    acf for blog page title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
   <section class="container pre-content text-center">
     <div class="row">
       <div class="col">
-        <h2>BLOG POST HEADLINE</h2>
+        <h2>{{ get_field('blog_page_title', (int)get_option( 'page_for_posts' )) }}</h2>
         @php
           echo get_field('content_top', (int)get_option( 'page_for_posts' )) ? sprintf( '<section class="container content-top">%s</section>', get_field('content_top', (int)get_option( 'page_for_posts' )) ) : '';
         @endphp
