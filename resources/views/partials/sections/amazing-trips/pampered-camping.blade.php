afcda062fe2ea381e2dd830b590e8f782f85e927
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-04-19 23:38:44 -0600

    remove 'book your adventure' button

--- resources/views/partials/sections/amazing-trips/pampered-camping.blade.php
+++ resources/views/partials/sections/amazing-trips/pampered-camping.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-12">
         <?php the_field('pampered_camping'); ?>
-        @include('partials.book-your-adventure', [ 'color' => 'white' ])
+        {{--@include('partials.book-your-adventure', [ 'color' => 'white' ])--}}
       </div>
     </div>
   </div>
