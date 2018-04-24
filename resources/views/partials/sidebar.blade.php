995ba3bd442ee742c09181f2d5d917611abf8ffc
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-04-24 11:16:13 -0600

    Update sidebar.blade.php

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,18 +1,1 @@
-<?php
-  $is_blog = is_home() || is_single() || is_category() ? true : false;
-?>
-<aside class="sidebar col-md-3 {{ $is_blog ? 'sidebar-blog' : 'sidebar-primary' }}">
-  <?php if( $is_blog ) { ?>
-    <section class="widget widget_text widget-follow-us">
-      <h3>Follow Us</h3>
-      <div class="textwidget">
-        @include('partials.social-media')
-      </div>
-    </section>
-    <?php
-    dynamic_sidebar('sidebar-blog');
-  } else {
-    dynamic_sidebar('sidebar-primary');
-  }
-  ?>
-</aside>
+
