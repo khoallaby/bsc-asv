7c46148ba8b448f4a36dc1ec2c4b6915fbf324c4
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-05-04 16:51:21 -0600

    Update book-your-adventure.blade.php

--- resources/views/partials/book-your-adventure.blade.php
+++ resources/views/partials/book-your-adventure.blade.php
@@ -1,1 +1,1 @@
-<a href="/contact/" class="btn btn-primary {{ $color or '' }}">Book Your Adventure</a>
+<a href="/guest-availability/" class="btn btn-primary {{ $color or '' }}">Book Your Adventure</a>
