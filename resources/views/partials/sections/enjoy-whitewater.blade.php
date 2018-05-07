256b19f300b1ed888bbcae95c1c539a528dec383
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-05-07 09:39:33 -0600

    Update enjoy-whitewater.blade.php

--- resources/views/partials/sections/enjoy-whitewater.blade.php
+++ resources/views/partials/sections/enjoy-whitewater.blade.php
@@ -4,7 +4,7 @@
             <h2>{{ the_field('enjoy_5050_left_title') }}</h2>
             <div class="row">
                 <div class="col-md-2 offset-md-1 text-center">
-                    <img src="@asset('images/logo-white.png')" class="img-fluid" />
+                    <img src="@asset('images/logo-white.png')" class="img-fluid" style="margin-bottom:20px;" />
                 </div>
                 <div class="col-md-8">
                   {{ the_field('enjoy_5050_left_content') }}
