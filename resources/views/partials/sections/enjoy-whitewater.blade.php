2b9a0aa7a33fa2e0927bef83f9fd5905a432dfe9
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-05-07 09:33:46 -0600

    Update enjoy-whitewater.blade.php

--- resources/views/partials/sections/enjoy-whitewater.blade.php
+++ resources/views/partials/sections/enjoy-whitewater.blade.php
@@ -1,9 +1,9 @@
 <section class="container-fluid enjoy-whitewater no-gutters">
     <div class="row">
-        <div class="col-md-6 enjoy-whitewater-left" style="background-image: url('{{ the_field('enjoy_5050_left_background') }}'); background-repeat:no-repeat;">
+        <div class="col-md-6 enjoy-whitewater-left" style="background-image: url('{{ the_field('enjoy_5050_left_background') }}'); background-repeat:no-repeat;background-size:cover;">
             <h2>{{ the_field('enjoy_5050_left_title') }}</h2>
             <div class="row">
-                <div class="col-md-2 offset-1 text-center">
+                <div class="col-md-2 offset-md-1 text-center">
                     <img src="@asset('images/logo-white.png')" class="img-fluid" />
                 </div>
                 <div class="col-md-8">
