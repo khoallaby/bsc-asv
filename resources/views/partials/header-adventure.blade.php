75ed67fe106c63ce6ec20d53a1a6defcc5aedd6f
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-05-14 11:01:15 -0600

    vcenter header-adventure text/image

--- resources/views/partials/header-adventure.blade.php
+++ resources/views/partials/header-adventure.blade.php
@@ -1,10 +1,10 @@
-<section class="container-fluid header-adventure">
+<section class="container-fluid header-adventure vcenter">
     <div class="container">
         <div class="row">
-            <div class="col-6 adventure-text">
+            <div class="col-6 adventure-text vcenter">
                 {{ $title or '' }}
             </div>
-            <div class="col-6">
+            <div class="col-6 adventure-image vcenter">
                 <img src="@asset('images/adventure.png')" class="img-fluid" style="margin-bottom:20px;" />
             </div>
         </div>
