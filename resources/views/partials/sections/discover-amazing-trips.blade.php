b48615c0ae29ef8528b9ec5c6915dc537a4efba6
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-05-16 23:41:47 -0600

    fix amazing-trips 5 icons not stacking on ios

--- resources/views/partials/sections/discover-amazing-trips.blade.php
+++ resources/views/partials/sections/discover-amazing-trips.blade.php
@@ -12,31 +12,31 @@
             </div>
         </div>
         <div class="row no-gutters">
-            <div class="col amazing-trip">
+            <div class="amazing-trip">
                 <a href="{{ home_url('our-amazing-trips/6-day-rafting/') }}">
                     <img src="@asset('images/icons/rafting.png')" />
                     <h3>6 Day Rafting</h3>
                 </a>
             </div>
-            <div class="col amazing-trip">
+            <div class="amazing-trip">
                 <a href="{{ home_url('our-amazing-trips/trout-fishing') }}">
                     <img src="@asset('images/icons/fishing.png')" />
                     <h3>Trout Fishing</h3>
                 </a>
             </div>
-            <div class="col amazing-trip">
+            <div class="amazing-trip">
                 <a href="{{ home_url('our-amazing-trips/kayak-support-trips') }}">
                     <img src="@asset('images/icons/kayak.png')" />
                     <h3>Kayak Support Trips</h3>
                 </a>
             </div>
-            <div class="col amazing-trip">
+            <div class="amazing-trip">
                 <a href="{{ home_url('our-amazing-trips/fall-cast-blast') }}">
                     <img src="@asset('images/icons/cast-blast.png')" />
                     <h3>Fall Cast & Blast</h3>
                 </a>
             </div>
-            <div class="col amazing-trip">
+            <div class="amazing-trip">
                 <a href="{{ home_url('our-amazing-trips/group-specialty') }}">
                     <img src="@asset('images/icons/group-specialty.png')" />
                     <h3>Group & Specialty</h3>
